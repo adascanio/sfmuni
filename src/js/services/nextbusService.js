8a21331bc4ab23543cd14c4a781ae48b0a99a595
adascanio <amedeo.dascanio@amadeus.com>
2016-12-14 18:43:37 +0100

    adding placeholder for nextbus

--- src/js/services/nextbusService.js
+++ src/js/services/nextbusService.js
@@ -1,6 +1,24 @@
 
 angular.module('NextBusService', []).factory('NextBusFactory', ['$http', function($http) {
 
+    function getRoutes() {
+        return $http.get('http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni');
+    };
+
+    function getRouteConfig(routeId) {
+        return $http.get('http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni&r=' + routeId);
+    };
+
+    function getStopPrediction (stopId) {
+        return $http.get('http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&stopId=' + stopId);
+    }
+
+    function getVehicleLocations(routeId) {
+        return $http.get('http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=sf-muni&t=1144953500233&routeId=' + routeId);
+        
+    }
+
+
     return {
         get : function() {
             return $http.get('/api/cards');
